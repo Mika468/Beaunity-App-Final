402b769b680301c3cf530c7e73c4990f6f1f1b17
Mikaela Foronda <mikaela.foronda@icloud.com>
2020-07-28 19:11:31 -0700

    Initial Commit

---
+++ Beaunity-App-Final/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Beaunity-App-Final
+//
+//  Created by Mikaela Foronda Zanipatin on 7/28/20.
+//  Copyright © 2020 Mikaela Foronda. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Beaunity-App-Final/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Beaunity-App-Final
+//
+//  Created by Mikaela Foronda Zanipatin on 7/28/20.
+//  Copyright © 2020 Mikaela Foronda. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
